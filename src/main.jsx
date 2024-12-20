696e1516abfe8ba438602631a74ab82247d829d1
João Sousa <joaosousa@MacBook-Pro-de-Beatriz-6.local>
2024-12-20 12:58:59 +0000

    Start responsive

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import {RouterProvider, createBrowserRouter} from "react-router-dom";
 //import { Authenticator } from "@aws-amplify/ui-react";
 
 // ReactDOM.createRoot(document.getElementById("root")).render(
@@ -12,8 +13,12 @@ import "./index.css";
 //     </React.StrictMode>
 // );
 
+const router = createBrowserRouter([
+    {path: '/', element: <App />}
+])
+
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router = {router} />
     </React.StrictMode>
 );
--- src/widgets/Card.jsx
+++ src/widgets/Card.jsx
@@ -1,12 +1,28 @@
 import classes from './Card.module.css';
+import { useState } from 'react';
+import { Image } from '@aws-amplify/ui-react';
 
 
 function Card() {
-    return (
-        <div className={classes.cardBackground}>
-            <div className={classes.card}>
 
-            </div>
+    // eslint-disable-next-line no-unused-vars
+    const [countDown, setCountDown] = useState('');
+
+    function getCountDown(){
+        setCountDown( '07-06-2025');
+    }
+
+    return (
+        <div>
+
+            <Image
+                alt=""
+                src="/assets/Header.png"
+                srcSet="/assets/Header.png 400w, /assets/Home_Desktop.png 1800w"
+                height="100%"
+                width="100%"
+            />
+
         </div>
     );
 }
