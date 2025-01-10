8377b1725383c14649ef4bd6af03dc021795d5e3
João Sousa <joaosousa@MacBook-Pro-de-Beatriz-6.local>
2025-01-10 16:09:38 +0000

    Alterada a BD

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -3,14 +3,14 @@ import { postConfirmation } from "../auth/post-confirmation/resource";
 
 const schema = a
     .schema({
-      UserProfile: a
-          .model({
-            email: a.string(),
-            profileOwner: a.string(),
-          })
-          .authorization((allow) => [
-            allow.ownerDefinedIn("profileOwner"),
-          ]),
+        WeddingInviteResponse: a.model({
+            name: a.string(),
+            phoneNumber: a.phone(),
+            isPlusOne: a.boolean(),
+            isAttending: a.boolean(),
+            foodRestrictions: a.string(),
+            musicSuggestions: a.string()
+        }).authorization(allow => [allow.publicApiKey()]),
     })
     .authorization((allow) => [allow.resource(postConfirmation)]);
 export type Schema = ClientSchema<typeof schema>;
